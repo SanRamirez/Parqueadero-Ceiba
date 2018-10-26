331bbfb316eacd25a2e7510ec3227e6989c3d501
Santiago Ramírez Acevedo - Ceiba Software <santiago.ramirez@ceiba.com.co>
2018-10-26 09:24:38 -0500

    init

---
+++ src/test/java/com/co/ceiba/example/ejemploSpring/EjemploSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.co.ceiba.example.ejemploSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EjemploSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
